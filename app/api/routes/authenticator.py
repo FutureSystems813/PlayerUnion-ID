b38fed37377f672c98a5ee976face06c4e3b5de3
m-weinzettl <054057@edu.szf.at>
2026-01-29 17:31:29 +0100

    password auf max 72 gesetzt

--- app/api/routes/authenticator.py
+++ app/api/routes/authenticator.py
@@ -3,6 +3,7 @@ from passlib.context import CryptContext
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_player_password(password):
+    safe_password = password[:72]
     return password_context.hash(password)
 
 def verify_player_password(plain_password: str, hashed_password: str):
